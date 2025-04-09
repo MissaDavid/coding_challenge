9528fcd4aaa9cba794ac52c7c161d072e609db8b
Melissa Tamplin <contact@meibee.dev>
2025-04-09 12:20:08 -0400

    fix(todo-app): dont parse json when response is 204, now delete works
    Signed-off-by: Mel Tamplin <contact@meibee.dev>

--- todo-app/src/actions/handleResponse.tsx
+++ todo-app/src/actions/handleResponse.tsx
@@ -4,5 +4,10 @@ export const handleResponse = async (response: Response) => {
         console.error(`API Error: ${response.status}`, errorText);
         throw new Error(`API Error: ${response.status} - ${errorText}`);
     }
+
+    if (response.status === 204) {
+        return null;
+    }
+    
     return await response.json();
 }
