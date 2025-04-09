3c248765f620d13e546ba5c473804857bd932c76
Melissa Tamplin <contact@meibee.dev>
2025-04-09 11:56:18 -0400

    fix(todos): make description field optional
    Signed-off-by: Mel Tamplin <contact@meibee.dev>

--- src/todo_coding_challenge/accounts/urls.py
+++ src/todo_coding_challenge/accounts/urls.py
@@ -5,6 +5,7 @@ from .views import RegisterView, UserDetailView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # will not regenerate the token but will blacklist it. == Logout
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="register"),
     path("me/", UserDetailView.as_view(), name="user_details"),
--- src/todo_coding_challenge/todos/models.py
+++ src/todo_coding_challenge/todos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class TodoItem(models.Model):
     owner = models.ForeignKey("accounts.User", on_delete=models.CASCADE, related_name="todos")
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(null=True, blank=True)
